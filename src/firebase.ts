daf72a1474a61df75aff89f3918ae6654ddc306c
Michael Scott Weber <mscottweber@gmail.com>
2025-02-15 10:19:08 -0600

    Removes some bilerplate; initializes firebase

---
+++ src/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyAhKp3feUmSWtEqlQ4YgP5jfrODuE4Rfjg',
+  authDomain: 'gftd-v1.firebaseapp.com',
+  projectId: 'gftd-v1',
+  storageBucket: 'gftd-v1.firebasestorage.app',
+  messagingSenderId: '1073412921952',
+  appId: '1:1073412921952:web:1c0d7f46c1fa78cfc88105',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
