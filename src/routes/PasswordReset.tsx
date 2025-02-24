7efbbceec0486bb82adaff567b7c2b1bcede24b3
Michael Scott Weber <mscottweber@gmail.com>
2025-02-24 11:24:34 -0600

    Adds styling to password reset page

--- src/routes/PasswordReset.tsx
+++ src/routes/PasswordReset.tsx
@@ -33,14 +33,22 @@ function PasswordReset() {
 
   if (emailSent) {
     return (
-      <main className=''>
+      <main className='bg-gray-50 h-screen flex flex-col items-center p-8'>
         <p>
           Check your email for a password reset link, and then{' '}
-          <NavLink to='/signin'>sign in</NavLink> with your new password.
+          <NavLink className='text-blue-500 cursor-pointer' to='/signin'>
+            sign in
+          </NavLink>{' '}
+          with your new password.
         </p>
         <p>
           Didn't receive an email?{' '}
-          <span onClick={handleResetEmail}>Send it again.</span>
+          <span
+            className='text-blue-500 cursor-pointer'
+            onClick={handleResetEmail}
+          >
+            Send it again.
+          </span>
         </p>
       </main>
     );
@@ -48,28 +56,34 @@ function PasswordReset() {
 
   if (!emailSent) {
     return (
-      <main className='PasswordReset'>
-        <h1>Password Reset</h1>
-        <p>
-          Enter the email associated with your account and click the button
-          below
-        </p>
+      <main className='bg-gray-50 h-screen flex flex-col items-center p-8'>
+        <h1 className='text-gray-900 text-3xl pb-8'>Password Reset</h1>
         <form
           onSubmit={(e) => {
             e.preventDefault();
             handleResetEmail();
           }}
+          className='bg-white container max-w-screen-sm p-8 border border-gray-200 rounded-xl shadow-md'
         >
-          <input
-            type='email'
-            id='email-address'
-            name='email'
-            required
-            placeholder='Email Address'
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-          <button>Send Password Reset Email</button>
+          <div className='flex flex-col mb-5'>
+            <label htmlFor='email-address'>
+              Enter the email associated with your account and click the button
+              below
+            </label>
+            <input
+              type='email'
+              id='email-address'
+              name='email'
+              required
+              placeholder='Email Address'
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              className='border border-gray-400 p-2 rounded-md'
+            />
+          </div>
+          <button className='px-6 py-2 bg-green-700 rounded-md text-white mb-5 cursor-pointer'>
+            Send Password Reset Email
+          </button>
         </form>
       </main>
     );
